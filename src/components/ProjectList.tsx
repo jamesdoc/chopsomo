4c59688f46acc41a992a91a2ae51089a93dd73ff
James Doc <hi@jamesdoc.com>
2024-11-07 22:35:05 +0000

    Add CTA to submit new projects

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -32,9 +32,17 @@ const ProjectList: React.FC = () => {
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">
-        Open Source Christian Projects
-      </h1>
+      <header className="md:flex items-center justify-between mb-4">
+        <h1 className="text-2xl font-bold">
+          Open Source Christian Projects
+        </h1>
+
+        <div>
+          <a className="text-blue-600 hover:underline" href="https://github.com/caabernathy/chopsomo#readme">
+            Add your own project to the list
+          </a>
+        </div>
+      </header>
 
       {/* Search Bar */}
       <div className="mb-4">
